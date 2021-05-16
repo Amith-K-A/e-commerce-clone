22bec7ca32b04acd9ab728060bef3ff3093860bc
Amith K A <amith.ka@intimetec.com>
2021-05-16 21:17:24 +0530

    added MERN stack support.

---
+++ routes/auth-routes.js
@@ -0,0 +1,14 @@
+const { verifySignUp } = require("../services");
+const controller = require("../controllers/auth-controller");
+
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
+
+  app.post("/signup", [verifySignUp.checkDuplicateUsernameOrEmail, verifySignUp.checkRolesExisted], controller.signup);
+
+  app.post("/signin", controller.signin);
+};
---
+++ server.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dbConfig = require("./config/db-config");
+const app = express();
+const db = require("./models");
+const Role = db.role;
+const path = require("path");
+
+app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("port", process.env.PORT || 4000);
+app.set("env", process.env.NODE_ENV);
+app.set("mongo", process.env.MONGODB_URI || `mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`);
+// // set port, listen for requests
+db.mongoose
+  .connect(app.get("mongo"), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch((err) => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
+
+require("./routes/auth-routes")(app);
+require("./routes/user-routes")(app);
+
+app.use("/", express.static(path.join(__dirname, 'client/build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', "index.html"))
+})
+
+if (app.get("env") === "production") {
+  app.use("*", express.static(path.join(__dirname, "../src", "build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../src", "build", "index.html"));
+  });
+}
+
+
+
+app.listen(app.get("port"), () => {
+  console.log(`Server is running on port`);
+});
+
